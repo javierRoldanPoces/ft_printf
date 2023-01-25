d1a0661d18528a95d99ab2094b5f0bc8f8f93bf8
Javier Roldan poces <javierroldanpoces@gmail.com>
2023-01-25 18:04:34 +0100

    Proyecto terminado, listo para entrega
    Solucionado errores de norminette, dividiendo función ft_printf en varias que detectan el parametro recibido.

--- ft_printf/ft_digit.c
+++ ft_printf/ft_digit.c
@@ -6,60 +6,96 @@
 /*   By: jroldan- <jroldan-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 20:50:29 by jroldan-          #+#    #+#             */
-/*   Updated: 2023/01/19 20:55:57 by jroldan-         ###   ########.fr       */
+/*   Updated: 2023/01/25 17:15:43 by jroldan-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-/*
-static void	recursiva_long(long nbr, long size, char *base, int *len)
-{
-	if (nbr >= size)
-	{
-		recursiva_long((nbr / size), size, base, len);
-		ft_putchar(base[nbr % size], length);
-	}
-	else
-		ft_putchar(base[nbr % size], length);
-}
-*/
 
-void	ft_putnbr_base(long nbr, char *base, int *len, char *p)
+void	ft_putnbr_d_i(int nbr, int *len)
+{
+	long	i;
+
+	i = nbr;
+	if (i < 0)
+	{
+		ft_putchar('-', len);
+		i = i * -1;
+	}
+	if (i > 9)
+	{
+		ft_putnbr_d_i(i / 10, len);
+		ft_putchar((i % 10) + 48, len);
+	}
+	if (i <= 9)
+	{
+		ft_putchar(i + '0', len);
+	}
+}
+
+void	ft_putnbr_unsigned_d(unsigned int nbr, int *len)
+{
+	long	i;
+
+	i = nbr;
+	if (i < 0)
+	{
+		ft_putchar('-', len);
+		i = i * -1;
+	}
+	if (i > 9)
+	{
+		ft_putnbr_d_i(i / 10, len);
+		ft_putchar((i % 10) + 48, len);
+	}
+	if (i <= 9)
+	{
+		ft_putchar(i + '0', len);
+	}
+}
+
+void	ft_putnbr_p(unsigned long nbr, char *base, int *len)
 {
 	unsigned long	size_base;
 	long			nbr_final[100];
 	int				i;
-	long			cast;
 
 	i = 0;
 	size_base = 0;
-	if (*p == 'u' || *p == 'x' || *p == 'X')
-		cast = (unsigned int)nbr;
-	if (*p == 'p')
-		cast = (unsigned long)nbr;
-	if (*p == 'd' || *p == 'i')
-	{
-		cast = (int)nbr;
-		if (cast < 0)
-		{
-			ft_putchar('-', len);
-			cast = cast * -1;
-		}
-	}
 	while (base[size_base])
 		size_base++;
-	while (cast)
+	if (nbr == 0)
+		ft_putchar('0', len);
+	while (nbr)
 	{
-		nbr_final[i] = cast % size_base;
-		cast = cast / size_base;
+		nbr_final[i] = nbr % size_base;
+		nbr = nbr / size_base;
 		i++;
 	}
 	while (--i >= 0)
 		ft_putchar(base[nbr_final[i]], len);
-	//return (len);
 }
 
-// unsigned int son todos lo x y la U ---echo
-// d e i son int c                    --echo
-// p unsignedl long					 --echo
-// controlar los null				-- echo en string
+void	ft_putnbr_hex(unsigned int nbr, char *base, int *len, char *p)
+{
+	unsigned long	size_base;
+	long			nbr_final[100];
+	int				i;
+
+	i = 0;
+	size_base = 0;
+	if (*p == 'X')
+		base = "0123456789ABCDEF";
+	while (base[size_base])
+		size_base++;
+	if (nbr == 0)
+		ft_putchar('0', len);
+	while (nbr)
+	{
+		nbr_final[i] = nbr % size_base;
+		nbr = nbr / size_base;
+		i++;
+	}
+	while (--i >= 0)
+		ft_putchar(base[nbr_final[i]], len);
+}
--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,113 +6,83 @@
 /*   By: jroldan- <jroldan-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/19 17:39:31 by jroldan-          #+#    #+#             */
-/*   Updated: 2023/01/19 20:55:31 by jroldan-         ###   ########.fr       */
+/*   Updated: 2023/01/25 17:33:10 by jroldan-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+static void	*condition_num(char *p, va_list args, int *len)
+{
+	if (*p == 'p')
+	{
+		ft_putstr("0x", len);
+		ft_putnbr_p(va_arg(args, unsigned long), "0123456789abcdef", len);
+		p += 1;
+	}
+	else if (*p == 'x' || *p == 'X')
+	{
+		ft_putnbr_hex(va_arg(args, unsigned int), "0123456789abcdef", len, p);
+		p += 1;
+	}
+	else if (*p == 'd' | *p == 'i')
+	{
+		ft_putnbr_d_i(va_arg(args, int), len);
+		p += 1;
+	}
+	return (p);
+}
+
+static void	*condition(char *p, va_list args, int *len)
+{
+	if (*p == 'c')
+	{
+		ft_putchar(va_arg(args, int), len);
+		p += 1;
+	}
+	else if (*p == 's')
+	{
+		ft_putstr(va_arg(args, char *), len);
+		p += 1;
+	}
+	else if (*p == 'u')
+	{
+		ft_putnbr_unsigned_d(va_arg(args, unsigned int), len);
+		p += 1;
+	}
+	else if (*p == '%')
+	{
+		ft_putchar('%', len);
+	}
+	else
+		condition_num(p, args, len);
+	return (p);
+}
+
 int	ft_printf(char const *fnt, ...)
 {
 	va_list	args;
-	int		len;
+	int		count;
+	int		*len;
 	char	*p;
-	char	*pn;
 
-	len = 0;
+	count = 0;
+	len = &count;
 	p = (char *)fnt;
 	va_start(args, fnt);
+	if ((write (1, "", 0) == -1))
+		return (-1);
 	while (*p != '\0')
 	{	
-		pn = p + 1;
-		if (*p == '%' && *pn == 'c')
+		if (*p == '%')
 		{
-			ft_putchar(va_arg(args, int), &len);
-			p += 1;
+			p++;
+			condition(p, args, len);
 		}
-		else if (*p == '%' && *pn == 's')
-		{
-			ft_putstr(va_arg(args, char *), &len);
-			p = p + 1;
-		}
-		else if (*p == '%' && *pn == '%')
-		{
-			ft_putchar('%', &len);
-			p += 1;
-		}
-		else if (*p == '%' && *pn == 'd')
-		{
-			ft_putnbr_base(va_arg(args, int), "0123456789", &len, pn);
-			p = p + 1;
-		}
-		else if (*p == '%' && *pn == 'i')
-		{
-			ft_putnbr_base(va_arg(args, int), "0123456789", &len, pn);
-			p = p + 1;
-		}
-
-/*			if (*p == '%' && *pn == 'x')
-		{
-			ft_putnbr_base(va_arg(args, char *), "0123456789abcdef", &len, pn);
-			p = p + 2;
-		}
-			if (*p == '%' && *pn == 'p')
-		{
-			ft_putstr("0x");
-			ft_putnbr_base(va_arg(args, char *), "0123456789abcdef", &len, pn);
-			p = p + 1;
-		}
-
-		if (*p == '%' && *pn == 'u')
-		{
-			ft_putnbr_base(va_arg(args, char *), "0123456789", &len, pn);
-			p = p + 2;
-		}
-		
-		if (*p == '%' && *pn == 'X')
-		{
-			ft_putnbr_base(va_arg(args, char *), "0123456789ABCDEF", &len, pn);
-			p = p + 1;
-		}*/
 		else
-			ft_putchar(*p, &len);
-		p = p + 1;
+			ft_putchar(*p, len);
+		p += 1;
 	}
 	va_end(args);
-	return (len);
+	return (*len);
 }
-/*
-int	main(void)
- {
- 	//char	c;
-
- 	//c = 'b';
-	//ft_printf("%c", '0'); 
-	//printf("%c", '0');
- 	//ft_printf("%s", "Hola juan");
-	//printf(" NULL %s NULL \n", NULL);
-	//ft_printf(" NULL %s NULL ", NULL);
-	//printf("\n");
-	//printf(" %c", '0' - 256);
-	//ft_printf(" %c", '0' - 256);
-	//printf(" %c", '0' - 256);
-
- 	
-	//printf("\nOriginal p : %p\n", &c);
- 	//ft_printf("\nMi funcion p : %p\n", &c);
- 	//printf("\nOriginal d: %d\n", LONG_MIN);
-	printf(" %d ", 214748364789);
- 	ft_printf("\nd: %d\n", 214748364789);
- 	printf("\nOriginal i: %i\n", -2147483647);
- 	ft_printf("\nMi funcion i: %i\n", -2147483647);
-}
-
-	//printf("\nOriginal u: %u\n", -39);
- 	//ft_printf("\nMi funcion u: %u\n", -39);
- 	//printf("\nOriginal x: %x\n", -9900);
- 	//ft_printf("\nMi funcion x: %x\n", -9900);
- 	//ft_printf("\nX: %X", -9900);
- 	//printf("\nOriginal X: %X\n", -9900);
-
- }
- */
